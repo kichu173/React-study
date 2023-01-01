a8ae2a4cc696d0058d071cfda7a2b0eb8b4b782b
Kiran Kumar K <kichu173.kk@gmail.com>
2023-01-01 19:23:03 +0530

    chapter -02 (Ignite our app)

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react"; // getting it from my node_modules
+import ReactDOM from "react-dom/client";
+
 // PURE REACT
 const heading = React.createElement(
   "h1",
@@ -5,7 +8,7 @@ const heading = React.createElement(
     id: "title", // <h1 id="title">Namaste Everyone from React!</h1>
   },
   "Namaste Everyone from React!"
-); // 'React' is a global variable which comes from the injected cdn js files. 'createElement' take three arguments - 1.element/tag we need to create, 2.empty object - takes props/html attributes, 3.text content/children to be displayed/inserted.
+); // 'React' is a global variable which comes from the injected cdn js files. 'createElement' take three arguments - 1.element/tag we need to create, 2.empty object - takes props/html attributes(it can take anything here and will show up in dom, ex: hello:"world"), 3.text content/children to be displayed/inserted.
 console.log(heading); // react element is a plain JS object
 
 const heading1 = React.createElement(
@@ -13,7 +16,7 @@ const heading1 = React.createElement(
   {
     id: "title1",
   },
-  "Heading 1"
+  "Heading 1 for parcel"
 );
 const heading2 = React.createElement(
   "h2",
